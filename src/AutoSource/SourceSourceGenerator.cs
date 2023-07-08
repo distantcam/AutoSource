4c3b2fea1ae6d26d09c93f937d29dae2c8fa2db6
Cameron MacFarland <distantcam@gmail.com>
2023-07-08 15:16:48 +0800

    Switching to incremental generator for better ux

--- src/AutoSource/SourceSourceGenerator.cs
+++ src/AutoSource/SourceSourceGenerator.cs
@@ -6,20 +6,20 @@ using Microsoft.CodeAnalysis.Text;
 namespace AutoSource;
 
 [Generator(LanguageNames.CSharp)]
-public class SourceSourceGenerator : ISourceGenerator
+public class SourceSourceGenerator : IIncrementalGenerator
 {
-    public void Initialize(GeneratorInitializationContext context)
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-    }
-
-    public void Execute(GeneratorExecutionContext context)
-    {
-        var assembly = Assembly.GetAssembly(typeof(SourceSourceGenerator));
-        var filenames = assembly.GetManifestResourceNames();
-        foreach (var filename in filenames)
+        context.RegisterPostInitializationOutput(static context =>
         {
-            context.AddSource(filename,
-                SourceText.From(assembly.GetManifestResourceStream(filename), Encoding.UTF8, canBeEmbedded: true));
-        }
+            var assembly = Assembly.GetAssembly(typeof(SourceSourceGenerator));
+            var filenames = assembly.GetManifestResourceNames();
+            foreach (var filename in filenames)
+            {
+                using var stream = assembly.GetManifestResourceStream(filename);
+                using var reader = new StreamReader(stream);
+                context.AddSource(filename, SourceText.From(reader.ReadToEnd(), Encoding.UTF8));
+            }
+        });
     }
 }
